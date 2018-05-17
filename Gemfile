75ab920f674d816585dcf3899b86221c64ba6bba
Robert Mosolgo <rdmosolgo@gmail.com>
2018-05-17 14:03:38 -0400

    Add graph

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@ ruby "2.5.0"
 source 'https://rubygems.org' do
   gem "rake"
   gem "rouge"
+  gem "graphviz"
 end
--- renderer.rb
+++ renderer.rb
@@ -1,6 +1,9 @@
 require "ripper"
 require "rouge"
 require "pp"
+require "graphviz"
+require "open3"
+require "base64"
 
 module App
   class Renderer
@@ -25,6 +28,44 @@ module App
       formatter = Rouge::Formatters::HTML.new
       lexer = Rouge::Lexers::Ruby.new
       formatter.format(lexer.lex(source))
+    end
+
+    def image_data
+      sexp = Ripper.sexp_raw(code)
+      graph = Graphviz::Graph.new
+      add_children(graph, sexp)
+      dot = graph.to_dot
+      data = nil
+      Open3.popen3("dot", "-Tpng") do |stdin, stdout, stderr, thd|
+        stdin.puts(dot)
+        stdin.close
+        # err = stderr.gets(nil)
+        # if err
+        #   warn(err)
+        # end
+        data = stdout.gets(nil)
+      end
+      Base64.encode64(data)
+    end
+
+    private
+
+    # Add the Ruby sexp to the Graphviz node
+    def add_children(node, sexp)
+      if sexp
+        if (!sexp.is_a?(Array)) || (sexp.is_a?(Array) && sexp.first.is_a?(Symbol))
+          name, *children = sexp
+          child_node = node.add_node(label: name.inspect)
+          if children.any?
+            children.each { |c| add_children(child_node, c) }
+          end
+        else
+          child_node = node.add_node(label: "[...]")
+          sexp.each { |c| add_children(child_node, c) }
+        end
+      else
+        node.add_node(label: "nil")
+      end
     end
   end
 end
